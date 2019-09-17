2cff04f5a688cc00a8468d8e7f8ea65c1c68af8f
Gocze Norbi <gnorbi951@gmail.com>
2019-09-17 14:00:55 +0200

    get_questions works by id-s now

--- data_manager.py
+++ data_manager.py
@@ -17,13 +17,17 @@ def get_questions(id=None):
     return csv_data[::-1]
 
 
-def get_answers():
+def get_answers(id=None):
     """Get the data from the answers csv as an Ordered_Dict list"""
     csv_data = []
-    with open('sample_data/answers.csv') as f:
+    with open('sample_data/answer.csv') as f:
         reader = csv.DictReader(f)
         for row in reader:
             d = dict(row)
+            if id is not None and id == d['id']:
+                return d
+
             csv_data.append(d)
+
     return csv_data
 
