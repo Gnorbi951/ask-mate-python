f65c9321b1f18bb0ab224d5c222b31e5ba9ad287
bbence98 <borbelybence98@gmail.com>
2019-09-18 07:40:34 +0200

    added exception handling; edited list.html

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 import util
 import data_manager
 import connection
@@ -18,10 +18,16 @@ def display_question(question_id: int):
     answer_data = data_manager.get_answers(question_id)
     if isinstance(answer_data, list):
         answer_data = {'message': 'No answer so far...'}
+    if isinstance(question_data, list):
+        return redirect('/error')
     return render_template('display_a_question.html',
                            question_data=question_data,
                            answer_data=answer_data
                            )
+
+@app.route('/error')
+def handle_exceptions():
+    return render_template('exception_handing.html')
 
 
 if __name__ == '__main__':
